c43cc59ebd042727c625f470185cf02a1e657850
aleksanderboe <Aleksanderbo10@gmail.com>
2024-10-18 18:23:13 +0200

    enable csrf protection

--- social_insecurity/config.py
+++ social_insecurity/config.py
@@ -21,4 +21,4 @@ class Config:
     SQLITE3_DATABASE_PATH = "sqlite3.db"  # Path relative to the Flask instance folder
     UPLOADS_FOLDER_PATH = "uploads"  # Path relative to the Flask instance folder
     ALLOWED_EXTENSIONS = {}  # TODO: Might use this at some point, probably don't want people to upload any file type
-    WTF_CSRF_ENABLED = False  # TODO: I should probably implement this wtforms feature, but it's not a priority
+    WTF_CSRF_ENABLED = True  # TODO: I should probably implement this wtforms feature, but it's not a priority
